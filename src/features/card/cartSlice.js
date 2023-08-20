cd3fc876dbb6593d3e7ef128c3d59d58ae99f933
Fatima Najafi <najafifatima762@gmail.com>
2023-08-20 12:39:12 +0430

    Add redux tutorial component

---
+++ src/features/card/cartSlice.js
@@ -0,0 +1,13 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const initialState = {
+  cartItems: [],
+  amount: 0,
+  total: 0,
+  isLoading: true,
+};
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+});
+console.log(cartSlice);
